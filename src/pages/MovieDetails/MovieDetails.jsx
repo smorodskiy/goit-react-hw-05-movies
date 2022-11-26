32996f336956bf932417d878289f6c720f1992f3
Igor Smorodsky <ingerard@gmail.com>
2022-11-26 18:23:05 +0200

    Movies details component

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieDetailsById } from 'utils/api';
+
+const PREFIX_POSTER_URL = 'https://image.tmdb.org/t/p/w500/';
+
 export const MovieDetails = () => {
-    return (<div>MovieDetails</div>)
-}
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetchMovieDetailsById(movieId).then(setMovie);
+  }, [movieId]);
+
+  console.log(movie);
+
+  if (!movie) return;
+
+  return (
+    <div>
+      <h2>{movie.title}</h2>
+      <img
+        src={`${PREFIX_POSTER_URL}/${movie.poster_path}`}
+        alt={`${movie.title}`}
+      />
+      <p>{movie.overview}</p>
+    </div>
+  );
+};
