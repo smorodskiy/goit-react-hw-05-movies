c681958d558bbb503639c2aa2439eb54771e5803
Igor Smorodsky <ingerard@gmail.com>
2022-11-26 18:22:51 +0200

    Movies component

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
 export const Movies = () => {
-    return (<div>Movies</div>)
-}
+  const [query, setQuery] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleChangeQuery = e => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmitQuery = e => {
+    e.preventDefault();
+    setSearchParams({ query });
+  };
+
+  useEffect(() => {});
+
+  return (
+    <div>
+      <form onSubmit={handleSubmitQuery}>
+        <input
+          type="test"
+          name="movie"
+          onChange={handleChangeQuery}
+          value={query}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
