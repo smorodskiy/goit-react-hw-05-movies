85b606af198ffefb5089dd3551685b90fa5b2f18
Igor Smorodsky <ingerard@gmail.com>
2022-11-27 23:11:58 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,8 @@ import { SharedLayout } from 'components/SharedLayout/SharedLayout';
 const Home = lazy(() => import('pages/Home/Home'));
 const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
 const Movies = lazy(() => import('pages/Movies/Movies'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Review/Review'));
 const NotFound = lazy(() => import('pages/NotFound/NotFound'));
 
 const App = () => {
@@ -15,8 +17,8 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />}>
-            {/* <Route path="cast" element={<Cast />} /> */}
-            {/* <Route path="reviews" element={<Reviews />} /> */}
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Route>
