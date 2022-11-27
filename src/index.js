583cc1831d2c52444a242725f42bf7f51f3be708
Igor Smorodsky <ingerard@gmail.com>
2022-11-27 23:17:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <BrowserRouter basename='/goit-react-hw-05-movies'>
+      <BrowserRouter basename='/goit-react-hw-05-movies/'>
         <App />
       </BrowserRouter>
     </ThemeProvider>
