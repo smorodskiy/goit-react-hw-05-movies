8eb4395b8d384b10c5750cc0e08406a7307cbb4a
Igor Smorodsky <ingerard@gmail.com>
2022-11-27 22:28:53 +0200

    Delete index.js

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export * from './LoadMoreButton/LoadMoreButton';
-export * from './ImageGallery/ImageGallery';
-export * from './ImageGalleryItem/ImageGalleryItem';
-export * from './Loader/Loader';
-export * from './Modal/Modal';
-export * from './Searchbar/Searchbar.jsx';
