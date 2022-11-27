a9dab1e3ce377c66b3dda3941d62d35145cd098c
Igor Smorodsky <ingerard@gmail.com>
2022-11-27 22:03:57 +0200

    styled

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
-import { Container } from 'components/App/App.styled';
 import { fetchMovie } from 'utils/api';
-import { MovieList } from 'components/MoviesList/MoviesList';
-import { NavBar } from 'components/NavBar/NavBar';
 
-export const Home = () => {
+import { Container } from 'components/App/App.styled';
+import { Title } from './Home.styled';
+
+import MovieList from 'components/MoviesList/MoviesList';
+
+const Home = () => {
   // Global states
   const [movies, setMovies] = useState([]);
 
@@ -16,8 +18,10 @@ export const Home = () => {
 
   return (
     <Container>
-      <NavBar />
+      <Title>Trending today</Title>
       <MovieList movies={movies} />
     </Container>
   );
 };
+
+export default Home;
---
+++ src/pages/Home/Home.styled.jsx
@@ -0,0 +1,7 @@
+// For stylish components
+import styled from 'styled-components';
+
+// CSS in JS
+export const Title = styled.h2`
+  padding: 0 20px;
+`;
